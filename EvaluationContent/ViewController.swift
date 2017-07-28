b1f2301a7cb3fd913187418cf6ed75bcff8f1391
MaxCheng <nokia6102@gmail.com>
2017-07-28 09:37:21 +0800

    做二個屬性對外給

--- EvaluationContent/ViewController.swift
+++ EvaluationContent/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource    {
     
+    var stars : Float =  4.0
+    var mydesc: String = "十分愉快的交談過程，對於工作內容及公司的介紹都十分清楚，對專業的要求可以清楚的說明並溝通！"
+    
     @IBOutlet weak var myTableView: UITableView!
     @IBOutlet weak var floatRatingView: FloatRatingView!
+    @IBOutlet weak var lblDesc: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         
+        self.floatRatingView.rating = stars
+        self.floatRatingView.editable = false
+        lblDesc.text = mydesc
     }
     
+    //MARK: Table
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -32,6 +39,16 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return cell
     }
     
-
+    // MARK: FloatRatingViewDelegate
+    func floatRatingView(_ ratingView: FloatRatingView, isUpdating rating:Float)
+    {
+        //        print( NSString(format: "%.2f", self.floatRatingView.rating) as String)
+    }
+    
+    func floatRatingView(_ ratingView: FloatRatingView, didUpdate rating: Float)
+    {
+        
+        print( NSString(format: "%.2f", self.floatRatingView.rating) as String)
+    }
 }
 
